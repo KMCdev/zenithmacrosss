06a78a55a7db185bc454248831217fa0a9623677
KMCdev <idkyoidkyo945@gmail.com>
2026-07-05 22:11:41 -0400

    first 5668776431268897ddkkvbmmmmmnad

--- src/components/vault/views/InterfaceView.jsx
+++ src/components/vault/views/InterfaceView.jsx
@@ -47,7 +47,7 @@ export default function InterfaceView() {
           onClick={() => setLightbox(src)}
           className="break-inside-avoid rounded-xl overflow-hidden border border-white/5 cursor-zoom-in">
           
-            <img src="/123" alt={`Interface screenshot ${i + 1}`} className="w-full h-auto block" loading="lazy" />
+          <img src={src} alt={`Interface screenshot ${i + 1}`} className="w-full h-auto block" loading="lazy" />
           </motion.div>
         )}
       </div>
